de634629d76b58079236d0ef7525eb3ec4f015f2
r0bertt <robefreestyle@gmail.com>
2020-01-08 20:00:49 +0200

    added index, route route

--- config/routes.php
+++ config/routes.php
@@ -15,11 +15,12 @@ Router::get("/topic/{topic}/edit", "TopicController@edit")->name('topic.edit');
 Router::patch("/topic/{topic}", "TopicController@update")->name('topic.update');
 Router::delete("/topic/{topic}", "TopicController@delete")->name('topic.delete');
 Router::put("/topic", "TopicController@store")->name('topic.store');
-Router::get("/topic/{topic}/examples", "ExampleController@show")->name('example.show');
+Router::get("/topic/{topic}/examples", "ExampleController@index")->name('example.index');
 Router::get("/topic/{topic}/examples/create", "ExampleController@create")->name('example.create');
-Router::patch("/topic/{topic}/examples/{example}", "ExampleController@update")->name('example.update');
+Router::patch("/topic/{topic}/examples/{example}", "ExampleController@show")->name('example.show');
 Router::get("/topic/{topic}/examples/{example}/edit", "ExampleController@edit")->name('example.edit');
 Router::delete("/topic/{topic}/examples/{example}", "ExampleController@delete")->name('example.delete');
+Router::put("/topi/{topic}/examples", "ExampleController@store")->name('example.store');
 
 
 
