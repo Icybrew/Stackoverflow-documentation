cd9d8470bff347a3c1a1119fcf768f34c5c7bf2e
r0bertt <robefreestyle@gmail.com>
2020-01-08 17:49:32 +0200

    Added routes to example pages and ExampleController.php is created

---
+++ app/controllers/ExampleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Examples;
+use App\Topic;
+use App\Core\Config;
+use App\Core\DB;
+use Symfony\Component\HttpFoundation\Request;
+
+class ExampleController extends controller
+{
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+}
