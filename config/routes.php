a1ea17ccf77ef660552fe763dd126d03bb130b97
r0bertt <robefreestyle@gmail.com>
2020-01-08 20:05:20 +0200

    added update example controller route and edited example show from patch to get

--- config/routes.php
+++ config/routes.php
@@ -17,8 +17,9 @@ Router::delete("/topic/{topic}", "TopicController@delete")->name('topic.delete')
 Router::put("/topic", "TopicController@store")->name('topic.store');
 Router::get("/topic/{topic}/examples", "ExampleController@index")->name('example.index');
 Router::get("/topic/{topic}/examples/create", "ExampleController@create")->name('example.create');
-Router::patch("/topic/{topic}/examples/{example}", "ExampleController@show")->name('example.show');
+Router::get("/topic/{topic}/examples/{example}", "ExampleController@show")->name('example.show');
 Router::get("/topic/{topic}/examples/{example}/edit", "ExampleController@edit")->name('example.edit');
+Router::patch("/topic/{topic}/examples/{example}", "ExampleController@update")->name('example.update');
 Router::delete("/topic/{topic}/examples/{example}", "ExampleController@delete")->name('example.delete');
 Router::put("/topi/{topic}/examples", "ExampleController@store")->name('example.store');
 
