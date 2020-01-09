cde852852846aca9192f9c6d8e756301265868d9
Aurelijus <aurelijus.posius@gmail.com>
2020-01-09 11:24:03 +0200

    Created static getData, get, set functions, made internal  private

--- app/core/Data.php
+++ app/core/Data.php
@@ -2,6 +2,47 @@
 
 namespace App\Core;
 
-class Data {
-    public static $DATA = [];
+/**
+ * Class Data
+ * @package App\Core
+ */
+class Data
+{
+    /**
+     * @var array
+     */
+    private static $_DATA = [];
+
+    /**
+     * Data constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public static function getData(): array
+    {
+        return self::$_DATA;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public static function get($name)
+    {
+        return self::$_DATA[$name] ?? null;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public static function set($name, $value)
+    {
+        self::$_gDATA[$name] = $value;
+    }
 }
