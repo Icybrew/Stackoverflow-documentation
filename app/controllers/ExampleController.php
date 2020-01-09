3300471d34a1a1bb5dbf65f8e29641f174702a67
r0bertt <robefreestyle@gmail.com>
2020-01-09 14:35:25 +0200

    Examples now can be created

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -35,9 +35,31 @@ class ExampleController extends controller
         $this->view('example/create', ['topic'=>$topic]);
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
+        $data = $request->request->all();
+        if(!isset($data['title']) || !isset($data['bodyhtml'])){
+            $this->view('errors/error404');
+        } else {
+            $query = [
+                "DocTopicId" => $id,
+                "Title" => $data['title'],
+                "BodyHtml" => $data['bodyhtml'],
+                "BodyMarkdown" => strip_tags($data['bodyhtml'])
+            ];
 
+            if(strlen($query['Title']) <= 0 || strlen($query['BodyHtml']) <= 0){
+                $this->view('errors/error404');
+            }
+
+
+            $id = Examples::insert($query);
+            $hostname = 'http://' . $request->server->get('HTTP_HOST');
+            $uri = $request->server->get('REQUEST_URI');
+            $redirect = $hostname . $uri . '/' . $id;
+            header("Location: $redirect");
+
+        }
     }
 
     public function delete($id)
