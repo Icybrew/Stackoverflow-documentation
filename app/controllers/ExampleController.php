974adae80f1d516e29777c8321066acb8b416fee
indruteok <indruteok@gmail.com>
2020-01-10 09:30:14 +0200

    [PCA-51] Create ExampleController delete function

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Core\Router;
 use App\Examples;
 use App\Topic;
 use App\Core\Config;
@@ -21,7 +22,10 @@ class ExampleController extends controller
         if (empty($topic)) {
             $this->view("errors/error404");
         } else {
-            $examples = Examples::select('*')->where('DocTopicId', '=', $topic->Id)->getAll();
+            $examples = Examples::select('*')
+                ->where('DocTopicId', '=', $topic->Id)
+                ->where('deleted', '=', 0)
+                ->getAll();
             $this->view('example/index', ['topic' => $topic, 'examples' => $examples]);
         }
     }
@@ -50,8 +54,25 @@ class ExampleController extends controller
        
     }
 
-    public function delete($id)
+    public function delete($id, Router $router, Request $request)
     {
+        $example = Examples::find($id);
 
+        if (empty($example)) {
+            $this->view("errors/error404");
+        } else {
+            $deleted = $example->deleted;
+            $topic = Topic::find($id);
+
+            if ($deleted == 0) {
+                DB::table('Examples')->where('Id', '=', $id)->update(['deleted' => 1]);
+
+                return redirect()->route('example.index', ['topic', 'topic' => $topic->Id], 'examples');
+
+            } else {
+                $this->view("errors/error404");
+            }
+        }
     }
+
 }
